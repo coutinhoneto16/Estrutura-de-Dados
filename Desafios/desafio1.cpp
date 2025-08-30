a75a69ad4ea5616610cdf8a31d67d9abd73dffd8
Neto Coutinho <coutinhoneto16@gmail.com>
2025-08-29 22:41:56 -0300

    Salvando alterações locais

---
+++ Desafios/desafio1.cpp
@@ -0,0 +1,17 @@
+//gerar um vetor com 50 numeros randomicos
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+
+    int V[50];
+    srand(100);
+
+    for (int i = 0; i < 50; i++){
+        V[i] = rand();
+        cout << V[i] << " ";
+    }
+}
